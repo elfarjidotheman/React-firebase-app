996346d1370e21c4c2e451d5b16b43a1262b1846
oatman <elfarjiotheman@gmail.com>
2024-07-25 20:15:16 +0100

    import { BrowserRouter, Route, Switch, NavLink } from 'react-router-dom'

--- src/App.js
+++ src/App.js
@@ -1,95 +1,41 @@
-import { useEffect, useState } from 'react'
-import Singlecard from './components/singlecard'
 import './App.css'
+import { BrowserRouter, Route, Switch, NavLink } from 'react-router-dom'
 
-  const cardImg = [
-
-    {"src": "/img/helmet-1.png", match: false},
-    {"src": "/img/potion-1.png", match: false},
-    {"src": "/img/ring-1.png", match: false},
-    {"src": "/img/scroll-1.png", match: false},
-    {"src": "/img/shield-1.png", match: false},
-    {"src": "/img/sword-1.png", match: false}
-  ]
+// page components
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Article from './pages/Article'
 
 function App() {
-
-  const [cards, setCards] = useState ([])
-  const [turns, setTurns] = useState ([])
-  const [choiceone, setChoiceone] = useState (null)
-  const [choice2, setchoice2] = useState (null)
-  const [desibel, setdesibel] = useState (false)
-
-  // shuffle cards:
-  const shuffleCards = () => {
-    const shuffledCards = [...cardImg, ...cardImg]
-    .sort(()=>Math.random()-0.5)
-    .map((card)=>({...card, id: Math.random()}))
-    setChoiceone(null)
-    setchoice2(null)
-    setCards(shuffledCards)
-    setTurns(0)
-  }
-  
-  // handle choice
-  const handleChoice = (card) => {
-    choiceone ? setchoice2(card) : setChoiceone(card)
-  }
-
-  // compaire cards
-  useEffect (()=>{
-    if (choiceone && choice2) {
-      setdesibel(true)
-      if ( choiceone.src === choice2.src ) {
-        setCards(prevcard => {
-          return prevcard.map(card => {
-            if (card.src === choiceone.src) {
-              return { ...card, match: true}
-            } else {
-                return card
-            }
-          })
-        })
-        restTurns()
-      } else {
-        setTimeout( () => restTurns() , 1000)
-      }
-    }
-  }, [choiceone,choice2])
-
-  console.log(cards)
-
-  // restTurns()
-  const restTurns = () => {
-    setChoiceone(null)
-    setchoice2(null)
-    setTurns(prevTurne => prevTurne + 1)
-    setdesibel(false)
-  }
-  // Auto starte
-  useEffect(()=>{
-    shuffleCards()
-  }, [])
   return (
     <div className="App">
-      <h1>Magic Match</h1>
-      <button onClick={shuffleCards}>New Game</button>
-      <div className="card-grid">
-        {cards.map(card => (
-          <Singlecard 
-            key={card.id} 
-            card={card} 
-            handleChoice={handleChoice} 
-            fliped= { card === choiceone || card === choice2 || card.match}
-            desibel = { desibel }
-          />
-        ))}
-      </div>
-      <p>
-        Num Turns : {turns}
-      </p>
+      <BrowserRouter>
+        <nav>
+          <h1>My Articles</h1>
+          <NavLink exact to="/">Home</NavLink>
+          <NavLink to="/about">About</NavLink>
+          <NavLink to="/contact">Contact</NavLink>
+        </nav>
+
+        <Switch>
+          <Route exact path="/">
+            <Home />  
+          </Route>
+          <Route path="/about">
+            <About />  
+          </Route>
+          <Route path="/contact">
+            <Contact />  
+          </Route>
+          <Route path="/articles/:id">
+            <Article />
+          </Route>
+        </Switch>
+
+      </BrowserRouter>
     </div>
   );
 }
 
-export default App;
+export default App
